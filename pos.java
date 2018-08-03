8498d2af48aefc580c040f80b9c897a8072f6f81
Panneerselvam D <35401027+reennaP@users.noreply.github.com>
2018-08-03 14:36:26 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,10 @@
+public class pos{
+public static void main(String [] arg){
+Scanner s= new Scanner(System.in);
+int n=s.nextInt();
+if(n>0){
+System.out.println("positive");}
+if(n<0){
+System.out.println("negative")
+}
+}}
