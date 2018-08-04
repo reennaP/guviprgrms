7366a3a2769557ed39d3017153541fb9325d474f
Panneerselvam D <35401027+reennaP@users.noreply.github.com>
2018-08-04 10:08:48 +0530

    Create oddeve.java

---
+++ oddeve.java
@@ -0,0 +1,10 @@
+public class oe{
+public static void main(String [] arg){
+Scanner s= new Scanner(System.in);
+int n=s.nextInt();
+if((n%2)==0){
+System.out.println("even number");}
+else{
+System.out.println("odd number");
+}
+}}
